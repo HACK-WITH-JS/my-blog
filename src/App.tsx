0dac6602758fd46925f69b00f3f69c88ad2fe893
HACK-WITH-JS <jungmini2040@gmail.com>
2024-01-08 10:07:04 +0900

    프로젝트에 사용될 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/posts">게시글 리스트</Link>
+        </li>
+        <li>
+          <Link to="/posts/:id">게시글 상세 페이지</Link>
+        </li>
+        <li>
+          <Link to="/posts/new">게시글 생성 페이지</Link>
+        </li>
+        <li>
+          <Link to="/posts/edit/:id">게시글 수정 페이지</Link>
+        </li>
+        <li>
+          <Link to="/profile">프로필 페이지</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>홈 페이지</h1>} />
+        <Route path="/posts" element={<h1>게시글 리스트</h1>} />
+        <Route path="/posts/:id" element={<h1>게시글 상세 페이지</h1>} />
+        <Route path="/posts/new" element={<h1>게시글 생성 페이지</h1>} />
+        <Route path="/posts/edit/:id" element={<h1>게시글 수정 페이지</h1>} />
+        <Route path="/profile" element={<h1>프로필 페이지</h1>} />
+        {/* 이상한 경로로 들어오면 홈 페이지로 이동하도록 변경 */}
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
   );
 }
 
