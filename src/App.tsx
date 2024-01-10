0146f703aca1db33f6101adeda1ebe49129c04fa
HACK-WITH-JS <jungmini2040@gmail.com>
2024-01-10 14:55:06 +0900

    다크모드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import "./App.css";
 import Router from "./components/Router";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ThemeContext from "context/ThemeContext";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
@@ -23,10 +25,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
       <Router isAuthenticated={isAuthenticated} />
-    </>
+    </div>
   );
 }
 
